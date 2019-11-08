15620550a76fd664a1c1b52c97760fbf7b51890b
Eitan Bar-David <eitansbd@gmail.com>
2019-11-08 14:39:56 +0000

    add TodoList validations

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,7 @@
 class TodoList < ApplicationRecord
+  has_many :todo_items, dependent: :destory
+  
+  validates :name, presence: true, 
+                  length: { maximum: 100 },
+                  uniqueness: { case_sensitive: false}
 end
