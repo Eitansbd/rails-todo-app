ff26a17d639d09d0e8e9874c3562321db0b220d6
Eitan Bar-David <eitansbd@gmail.com>
2019-11-08 15:01:22 +0000

    add controller files

---
+++ app/controllers/todo_lists_controller.rb
@@ -0,0 +1,26 @@
+class TodoListsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  def complete_all
+    
+  end
+end
