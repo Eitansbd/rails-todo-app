3956ee175c4be79921996546d6f4f66c7023918d
Eitan Bar-David <eitansbd@gmail.com>
2019-11-08 14:51:13 +0000

    add todo list model tests

--- test/models/todo_list_test.rb
+++ test/models/todo_list_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class TodoListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @todo_list = TodoList.new(name: "Test List")
+  end
+  
+  test "should be valid" do
+    assert @todo_list.valid?
+  end
+  
+  test "name should be present" do
+    @todo_list.name = ""
+    assert_not @todo_list.valid?
+  end
+  
+  test "name should be less than 100 characters" do 
+    @todo_list.name = 'a' * 101
+    assert_not @todo_list.valid?
+  end
+  
+  test "name should be unique" do
+    @duplicate_todo_list = @todo_list.dup
+    @duplicate_todo_list.name.upcase!
+    @todo_list.save
+    
+    assert_not @duplicate_todo_list.valid?
+  end
 end
