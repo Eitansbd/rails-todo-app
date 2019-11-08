5ad11a8ec8fe041da0d0c4253090e33bb59a808c
Eitan Bar-David <eitansbd@gmail.com>
2019-11-08 16:57:22 +0000

    nest items in listst

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   
   root 'todo_lists#index'
-  resources :todo_lists
+  resources :todo_lists do
+    resources :todo_items, only: [:create, :destroy]
+  end
   
-  post '/todo_list/complete_all'
+  #get '/todo_lists/:todo_list_id/complete_all' to: 'todo_lists#complete_all'
+  
+  # post '/todo_lists/:todo_list_id/todo_items/:todo_item_id/toggle' to: 'todo_items#toggle'
+  
 end
