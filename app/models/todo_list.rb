4b1b48ede3772e7cae54d24d220ff9d28c7f9c62
Eitan Bar-David <eitansbd@gmail.com>
2019-11-08 14:49:43 +0000

    fix typo on destroy

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,5 @@
 class TodoList < ApplicationRecord
-  has_many :todo_items, dependent: :destory
+  has_many :todo_items, dependent: :destroy
   
   validates :name, presence: true, 
                   length: { maximum: 100 },
