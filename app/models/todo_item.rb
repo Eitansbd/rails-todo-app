4a89873a65fdbeeeb853687d0cedaff1770ccb53
Eitan Bar-David <eitansbd@gmail.com>
2019-11-08 14:39:30 +0000

    add TodoItem validations

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,7 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+  
+  validates :name, presence: true, 
+                   length: { maximum: 100 }
+                   uniqueness: { scope: :todo_list_id }
 end
