6bfb776b67fb12f0b565863270e1088ec71f15a3
Eitan Bar-David <eitansbd@gmail.com>
2019-11-10 02:19:56 +0000

    add db cleaner

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
+require 'database_cleaner'
+
+DatabaseCleaner.clean_with(:truncation)
+
 3.times do |num|
   list = TodoList.create(name: "list #{num + 1}")
   
   3.times do |num|
-    list.todo_items.create(name: "item #{num + 1}")
+    list.todo_items.create(name: "item #{num + 1}", complete: completed)
   end
   
 end
