05d331a78292482cae27cc60e8212668ec4435b1
Eitan Bar-David <eitansbd@gmail.com>
2019-11-10 17:22:41 +0000

    add destroy

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -3,6 +3,7 @@ class TodoItemsController < ApplicationController
     todo_item = new_todo_item
     
     todo_item.save
+    flash[:success] = "You created the todo item"
     
     redirect_to todo_list_url(params[:todo_list_id]) 
   end
@@ -11,6 +12,13 @@ class TodoItemsController < ApplicationController
   end
 
   def destroy
+    todo_item = TodoItem.find_by(id: params[:id], todo_list_id: params[:todo_list_id])
+    
+    todo_item.destroy
+    
+    flash[:success] = "Todo item deleted"
+    
+    redirect_to todo_list_url(params[:todo_list_id])
   end
   
   private
