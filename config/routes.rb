cb97ebc4c4b1e155a11b1d9020f62c9b3a1d41a7
Eitan Bar-David <eitansbd@gmail.com>
2019-11-10 19:39:45 +0000

    fix typo todo lists

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   
   root 'todo_lists#index'
   resources :todo_lists do
-    resources :todo_items, only: [:create, :destroy]
+    resources :todo_items, only: [:create, :destroy] do
+      patch :toggle, to: 'todo_items#toggle'
+    end
+    patch :complete_all, to: 'todo_lists#complete_all'
   end
-  
-  #get '/todo_lists/:todo_list_id/complete_all' to: 'todo_lists#complete_all'
-  
-  # post '/todo_lists/:todo_list_id/todo_items/:todo_item_id/toggle' to: 'todo_items#toggle'
+
   
 end
