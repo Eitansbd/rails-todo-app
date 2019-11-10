9b7b26b2b34d17e2022b89735461660086facb9f
Eitan Bar-David <eitansbd@gmail.com>
2019-11-10 21:05:44 +0000

    whitespace removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   root 'todo_lists#index'
+  
   resources :todo_lists do
     resources :todo_items, only: [:create, :destroy] do
       patch :toggle, to: 'todo_items#toggle'
@@ -8,5 +9,4 @@ Rails.application.routes.draw do
     patch :complete_all, to: 'todo_lists#complete_all'
   end
 
-  
 end
