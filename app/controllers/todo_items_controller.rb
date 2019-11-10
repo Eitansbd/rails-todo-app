65c0cbc347c5c4f4cc33ee2e283841086d8620ed
Eitan Bar-David <eitansbd@gmail.com>
2019-11-10 16:57:12 +0000

    add create action

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,10 @@
 class TodoItemsController < ApplicationController
   def create
+    todo_item = new_todo_item
+    
+    todo_item.save
+    
+    redirect_to todo_list_url(params[:todo_list_id]) 
   end
 
   def toggle
@@ -7,4 +12,12 @@ class TodoItemsController < ApplicationController
 
   def destroy
   end
+  
+  private
+    def new_todo_item
+      TodoItem.new do |item|
+        item.name = params[:todo_item][:name]
+        item.todo_list_id = params[:todo_list_id]
+      end
+    end
 end
