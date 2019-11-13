8e205f80417fcecd685ba3ec527ab63dfe1a7db8
Eitan Bar-David <eitansbd@gmail.com>
2019-11-13 19:42:39 +0000

    change message for errors

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,28 +2,32 @@ class TodoItemsController < ApplicationController
   def create
     todo_item = new_todo_item
     
-    todo_item.save
-    flash[:success] = "You created the todo item"
-    
-    redirect_to todo_list_url(params[:todo_list_id]) 
+    if todo_item.save
+      flash[:success] = "You created the todo item"
+      
+      redirect_to todo_list_url(params[:todo_list_id]) 
+    else 
+      flash[:error] = todo_item.errors.full_messages.first
+      render '/new'
+    end
   end
 
   def toggle
     todo_item = TodoItem.find_by(id: params[:todo_item_id], todo_list_id: params[:todo_list_id])
     
     todo_item.toggle!(:completed)
-    
-    
+
     redirect_to todo_list_url(params[:todo_list_id])
   end
 
   def destroy
     todo_item = TodoItem.find_by(id: params[:id], todo_list_id: params[:todo_list_id])
     
-    todo_item.destroy
-    
-    flash[:success] = "Todo item deleted"
-    
+    if todo_item.destroy
+      flash[:success] = "Todo item deleted"
+    else
+      flash[:error] = "There was an error deleting the todo"
+    end
     redirect_to todo_list_url(params[:todo_list_id])
   end
   
